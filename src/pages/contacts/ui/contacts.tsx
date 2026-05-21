4a8cf4bcd9cc270443d7b2ccfe8cf8640bc1e7a8
Bitsulov <roma.bitsulov@mail.ru>
2026-05-21 20:11:03 +0300

    implement contacts page
    Wired ContactsForm with user data from Redux.
    Added page title and meta description for SEO.

--- src/pages/contacts/ui/contacts.tsx
+++ src/pages/contacts/ui/contacts.tsx
@@ -1,11 +1,23 @@
 import {Layout} from "widgets/layout";
-import {Develop} from "widgets/develop";
+import {useSelector} from "react-redux";
+import {selectUserInfo} from "entities/user";
+import {ContactsForm} from "widgets/contactsForm";
+import {useTranslation} from "react-i18next";
 
-/** Страница контактов (в разработке). */
+/**
+ * Страница связи с администрацией.
+ *
+ * Загружает данные текущего пользователя из Redux и передаёт их в {@link ContactsForm}.
+ */
 export const Contacts = () => {
+    const { t } = useTranslation();
+    const userInfo = useSelector(selectUserInfo);
+
 	return (
 		<Layout>
-            <Develop />
+            <title>{t("titles.contacts")}</title>
+            <meta name="description" content={t("description.contacts")} />
+            <ContactsForm userInfo={userInfo} />
 		</Layout>
 	)
 }
