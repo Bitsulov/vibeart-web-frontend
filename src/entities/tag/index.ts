efe9c1cdf58c3282191484097481e3efe6cfcdd7
Bitsulov <roma.bitsulov@mail.ru>
2026-05-21 14:35:38 +0300

    add communityTagsMock to entities/tag
    Mock tag list for community creation form used by
    createCommunityWidget and its unit tests.

--- src/entities/tag/index.ts
+++ src/entities/tag/index.ts
@@ -1,3 +1,3 @@
 export type {TagType} from "./lib/types";
 export {createTag} from "./model/createTag";
-export {postTagsMock} from "./const/mockConst";
+export {postTagsMock, communityTagsMock} from "./const/mockConst";
