85243280cd28778342730a8455c63107980e6e02
Bitsulov <roma.bitsulov@mail.ru>
2026-05-21 14:38:03 +0300

    wire up createCommunity page
    Render CreateCommunityWidget with userInfo from Redux store
    and communityTagsMock as tag list. Store partial community
    state locally for preview updates.

--- src/pages/createCommunity/ui/createCommunity.tsx
+++ src/pages/createCommunity/ui/createCommunity.tsx
@@ -1,11 +1,34 @@
 import {Layout} from "widgets/layout";
-import {Develop} from "widgets/develop";
+import {useTranslation} from "react-i18next";
+import {useSelector} from "react-redux";
+import {selectUserInfo} from "entities/user";
+import {CreateCommunityWidget} from "widgets/createCommunityWidget";
+import {useState} from "react";
+import type {CommunityType} from "entities/community";
+import {communityTagsMock} from "entities/tag";
 
-/** Страница создания сообщества (в разработке) */
+/**
+ * Страница создания нового сообщества.
+ *
+ * Получает данные текущего пользователя из Redux и передаёт их в {@link CreateCommunityWidget}.
+ * Список тегов временно использует мок-данные {@link communityTagsMock}.
+ * Состояние формируемого сообщества хранится локально и обновляется при предпросмотре.
+ */
 export const CreateCommunity = () => {
+    const { t } = useTranslation();
+    const userInfo = useSelector(selectUserInfo);
+    const [communityInfo, setCommunityInfo] = useState<Partial<CommunityType>>({});
+
 	return (
 		<Layout isSmallTitle={true}>
-			<Develop />
+            <title>{t("titles.communityCreate")}</title>
+            <meta name="description" content={t("description.communityCreate")} />
+            <CreateCommunityWidget
+                tagsList={communityTagsMock}
+                communityInfo={communityInfo}
+                setCommunityInfo={setCommunityInfo}
+                userInfo={userInfo}
+            />
 		</Layout>
 	)
 }
